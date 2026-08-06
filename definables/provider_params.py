20839ac477400812de4bad42d324b98985b8c981
dsb589 <danielsbegemann@gmail.com>
2026-08-07 00:49:29 +0100

    add keyword arguments to Connecticut, New York, and Massachusetts dictionaries; leave Nonprofit dictionary blank for now

--- definables/provider_params.py
+++ definables/provider_params.py
@@ -1,6 +1,35 @@
 PROVIDER_PARAMS = {
-    "Connecticut": {},
-    "Massachusetts": {},
-    "New York": {},
+    "Connecticut": {
+        "endpoint_url": "https://data.ct.gov/resource/n7gp-d28j.json",
+        "where_cond": "lower(status)='active'",
+        "select_cond": """
+            id as source_id,
+            name as legal_name,
+            status as entity_status,
+            date_registration
+        """,
+        "batch_size": 5000,
+        "source": "CT_BUSINESS_REGISTRY",
+        "state": "CT",
+        "source_type": "socrata_api",
+        "id_col": "id"},
+    "Massachusetts": {
+        "endpoint_url": "https://corp.sec.state.ma.us/corpweb/CorpSearch/CorpSearch.aspx",
+        "source": "MA_BUSINESS_REGISTRY",
+        "state": "MA",
+        "source_type": "selenium",
+        "search_terms": list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+        "batch_size": 100},
+    "New York": {
+        "endpoint_url": "https://data.ny.gov/resource/n9v6-gdp6.json",
+        "select_cond": """
+            dos_id as source_id,
+            current_entity_name as legal_name
+        """,
+        "batch_size": 5000,
+        "source": "NY_BUSINESS_REGISTRY",
+        "state": "NY",
+        "id_col": "dos_id"
+        },
     "Nonprofits": {}
-    }
+}
