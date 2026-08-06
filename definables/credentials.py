4beb699f3fb968a3c5b446814df046776a995730
dsb589 <danielsbegemann@gmail.com>
2026-08-07 00:36:10 +0100

    Blank version of credentials.py - to be populated by the end user

---
+++ definables/credentials.py
@@ -0,0 +1,13 @@
+"""
+jobsfromthesource requires making accounts on different sites. Enter your 
+credentials into the fields below and amend them locally. Do not commit or
+push your credentials to the repository.
+"""
+
+# Supabase Details
+SUPABASE_DIRECT_URL=""
+SUPABASE_DATABASE_URL = ""
+SUPABASE_API_KEY = ""
+
+# Gemini Details
+GEMINI_API_KEY = ""
