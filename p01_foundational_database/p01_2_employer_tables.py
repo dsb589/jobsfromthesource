a831fde21ff3e8cafe39943aa3dea5e68c2067ef
dsb589 <danielsbegemann@gmail.com>
2026-08-08 22:38:01 +0100

    Add docstrings to all functions

--- p01_foundational_database/p01_2_employer_tables.py
+++ p01_foundational_database/p01_2_employer_tables.py
@@ -2,18 +2,50 @@ import pandas as pd
 
 
 def clean_employer_dataframe(df, apply_filters=True):
+    """
+    Function to remove employers from df that are unlikely to be true
+    employers.
+    returns df
+    """
     
 def remove_financial_entities(df, apply_filters=True):
+    """
+    Function to remove financial entities from df that are unlikely to be true
+    employers.
+    returns df
+    """
     
 def apply_employer_filters(df, apply_filters=True):
+    """
+    Function to remove additional entities from df that are unlikely to be true
+    employers.
+    returns df
+    """
 
 def employer_priority_score(df):
+    """
+    Function to assgn priority scores to employers based on key words in their
+    name that indicate whether they are or are not employers
+    """
+
 
 def create_website_queue(df, registry_size_cutoff=100000, apply_filters=True):
+    """
+    Function to remove additional entities from df that are unlikely to be true
+    employers.
+    returns df
+    """
     
 def deduplicate_employers(df, apply_filters=True):
+    """
+    Function to normalize names in the df and deduplicate them
+    returns df
+    """
     
 def analyze_website_queue(df):
+    "Function to print basic analysis of cleaned df"
     
 def run_first_cleaning_pass(df, apply_filters=True):
-
+    """
+    Master function that runs through all other functions in file
+    """
