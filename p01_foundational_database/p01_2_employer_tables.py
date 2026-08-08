4deabaf74f906b98865d5e2b287e0f939e882fd7
dsb589 <danielsbegemann@gmail.com>
2026-08-08 22:16:08 +0100

    initialize all functions in p01_2_employer_tables.py: clean_employer_dataframe, remove_financial_entities, apply_employer_filters, employer_priority_score, create_website_queue, deduplicate_employers, analyze_website_queue, and run_first_cleaning_pass

---
+++ p01_foundational_database/p01_2_employer_tables.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+
+def clean_employer_dataframe(df, apply_filters=True):
+    
+def remove_financial_entities(df, apply_filters=True):
+    
+def apply_employer_filters(df, apply_filters=True):
+
+def employer_priority_score(df):
+
+def create_website_queue(df, registry_size_cutoff=100000, apply_filters=True):
+    
+def deduplicate_employers(df, apply_filters=True):
+    
+def analyze_website_queue(df):
+    
+def run_first_cleaning_pass(df, apply_filters=True):
+
