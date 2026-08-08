f6f688748b75b6120e97455b41873c2621139db4
dsb589 <danielsbegemann@gmail.com>
2026-08-08 11:36:03 +0100

    add nonprofit categories - 501c codes and NTEE categories - to constants

--- definables/constants.py
+++ definables/constants.py
@@ -6,3 +6,13 @@ Created on Sat Aug  8 10:03:10 2026
 """
 
 STATES = ["CT", "MA", "NY"]
+
+# ProPublica NTEE major groups
+NONPROFIT_NTEE_CATEGORIES = list(range(1, 11))
+
+# ProPublica 501(c) subsection codes
+NONPROFIT_501C_CODES = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18,
+    19, 21, 22, 23, 25, 26, 27, 28, 92
+]
