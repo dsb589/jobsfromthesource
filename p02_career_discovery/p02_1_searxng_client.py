026bcd4ab5daf166351cef185872dd74590eb090
dsb589 <danielsbegemann@gmail.com>
2026-08-09 10:51:58 +0100

    create search_searxng function to run searches through searxng

--- p02_career_discovery/p02_1_searxng_client.py
+++ p02_career_discovery/p02_1_searxng_client.py
@@ -1,2 +1,32 @@
 import requests
 from definables import constants as dfn
+
+def search_searxng(query, num_results=10):
+    """
+    Query local SearXNG instance.
+    Returns:
+        list of search result dictionaries
+    """
+    # Set parameters + headers for searxng search
+    params = {
+        "q": query,
+        "format": "json",
+        "engines": "bing"
+    }
+    headers = {
+        "User-Agent": "CareerDiscoveryBot/1.0"
+    }
+    # Submit request
+    response = requests.get(
+        dfn.SEARXNG_URL,
+        params=params,
+        headers=headers,
+        timeout=30
+    )
+    # Get response & convert it to data from json
+    response.raise_for_status()
+    data = response.json()
+    # Return results from data; if no results, return blank list.
+    results = data.get("results", [])
+    # Return number of search results for the search, pre-specified (10 by default)
+    return results[:num_results]
