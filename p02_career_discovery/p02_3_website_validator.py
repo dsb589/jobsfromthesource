dd28d8eb34656866eb79194f8152bf5b7ec52ad2
dsb589 <danielsbegemann@gmail.com>
2026-08-10 00:00:32 +0100

    initialize functions that call and make requests to ollama

--- p02_career_discovery/p02_3_website_validator.py
+++ p02_career_discovery/p02_3_website_validator.py
@@ -8,3 +8,9 @@ from bs4 import BeautifulSoup
 # start session to check websites
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0"})
+
+def ollama_available():
+    
+def wait_for_ollama():
+
+def ask_ollama():
