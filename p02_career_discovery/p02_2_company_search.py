1e5907c46ac71e9d1cef2eee7be8ec31a0fec295
dsb589 <danielsbegemann@gmail.com>
2026-08-09 10:57:57 +0100

    initialize function to check if docker is running and if it's not running, start it.

--- p02_career_discovery/p02_2_company_search.py
+++ p02_career_discovery/p02_2_company_search.py
@@ -6,3 +6,10 @@ from urllib.parse import urlparse
 import re
 from cd1_searxng_client import search_searxng
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def ensure_docker_running(timeout=120, check_interval=2):
+    """
+    In order for SearXNG to work, a Docker instance must be running.
+    This function confirms wether Docker is running, and if it isn't running,
+    it starts it.
+    """
